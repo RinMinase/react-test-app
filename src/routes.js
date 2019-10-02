4edac2d46021d9aee2832753e01f290bd7880183
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-03 01:54:37 +0800

    Added test routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,25 @@
-import React from "react";
-import { Router, Route } from "react-router-dom";
+import React, { Suspense, lazy } from "react";
+import { Router, Route, Link } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
-import App from "./index";
+const App = lazy(() => import('./index'));
+const Home = lazy(() => import('./home'));
+const Login = lazy(() => import('./login'));
 
 export default function Routes() {
 	return(
 		<Router history={createBrowserHistory()}>
-			<Route exact path="/" component={App}></Route>
+			<Suspense fallback={<div>Loading...</div>}>
+				<div>
+					<ul>
+						<li><Link to="/home">Home</Link></li>
+						<li><Link to="/login">Login</Link></li>
+					</ul>
+				</div>
+				<App />
+				<Route exact path="/home" component={Home} />
+				<Route exact path="/login" component={Login} />
+			</Suspense>
 		</Router>
 	)
 }
