ced26e02fa74d3a73313c4612718696a4cb35147
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-03 01:53:24 +0800

    Improved code readability

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 import css from "./index.scss";
 import Routes from "./routes";
 
-export default class App extends React.Component {
+export default class App extends Component {
 	render () {
 		return(
 			<div className={css.test}>Hello World!</div>
