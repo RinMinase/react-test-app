6c39f39ede0c998ea2fe9e0c5b1aba05d1cbe483
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-03 17:09:58 +0800

    Added links to main component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
 
 import css from "./index.scss";
 import Routes from "./routes";
@@ -7,7 +8,13 @@ import Routes from "./routes";
 export default class App extends Component {
 	render () {
 		return(
-			<div className={css.test}>Hello World!</div>
+			<div>
+				<p className={css.test}>Hello World!</p>
+				<ul>
+					<li><Link to="/home">Home</Link></li>
+					<li><Link to="/login">Login</Link></li>
+				</ul>
+			</div>
 		);
 	}
 }
