651782c385e736006fddbc3aa8f46261cf0acd26
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-04 11:53:32 +0800

    Imported navigation bar to main component

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
 
 import css from "./index.scss";
+import Navbar from "./core/navbar";
 import Routes from "./routes";
 
 export default class App extends Component {
 	render () {
 		return(
 			<div>
-				<p className={css.test}>Hello World!</p>
-				<ul>
-					<li><Link to="/home">Home</Link></li>
-					<li><Link to="/login">Login</Link></li>
-				</ul>
+				<Navbar />
+				<div className="container">
+					<p className={css.test}>Hello World!</p>
+					<ul>
+						<li><Link to="/home">Home</Link></li>
+						<li><Link to="/login">Login</Link></li>
+					</ul>
+				</div>
 			</div>
 		);
 	}
