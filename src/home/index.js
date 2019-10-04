b8b7a57603d05d549c3abd5b1726687e6ff73803
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-04 16:03:22 +0800

    Imported stylesheet in home component

--- src/home/index.js
+++ src/home/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 
+import style from "./style.scss";
+
 export default class Home extends Component {
 	render () {
-		return(<div>Home Component loaded!</div>);
+		return(<div className={style.test}>Home Component loaded!</div>);
 	}
 }
