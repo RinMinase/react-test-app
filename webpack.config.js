21b5508f9a1005d7b7f4260d4450425f6062d689
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-09 16:19:18 +0800

    Modified to function webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,68 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-	entry: "./src/index.js",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "index_bundle.js"
-	},
-	module: {
-		rules: [{
-			test: /\.(js)$/,
-			loader: "babel-loader",
-			query: { compact: false }
-		}, {
-			/** This applies to all scss except `global.scss` */
-			test: /^((?!global).)*scss$/,
-			loader: [ "style-loader", {
-				loader: "css-loader",
-				options: {
-					modules: true,
-					sourceMap: true,
-				}
+module.exports = (_env, arg) => {
+	const webpackConfig = {
+		entry: "./src/index.js",
+		output: {
+			path: path.resolve(__dirname, "dist"),
+			filename: "index_bundle.js"
+		},
+		module: {
+			rules: [{
+				test: /\.(js)$/,
+				loader: "babel-loader",
+				query: { compact: false }
 			}, {
-				loader: "sass-loader",
-				options: { sourceMap: true }
-			}]
-		}, {
-			/** This applies to the main stylesheet named `global.scss` */
-			test: /global\.scss$/,
-			loader: [ "style-loader", {
-				loader: "css-loader",
-				options: { sourceMap: true }
+				/** This applies to all scss except `global.scss` */
+				test: /^((?!global).)*scss$/,
+				loader: [ "style-loader", {
+					loader: "css-loader",
+					options: {
+						modules: true,
+						sourceMap: true,
+					}
+				}, {
+					loader: "sass-loader",
+					options: { sourceMap: true }
+				}]
 			}, {
-				loader: "sass-loader",
-				options: { sourceMap: true }
+				/** This applies to the main stylesheet named `global.scss` */
+				test: /global\.scss$/,
+				loader: [ "style-loader", {
+					loader: "css-loader",
+					options: { sourceMap: true }
+				}, {
+					loader: "sass-loader",
+					options: { sourceMap: true }
+				}]
 			}]
-		}]
-	},
-	mode: "development",
-	devServer: {
-		open: false,
-		port: 3000,
-		historyApiFallback: true
-	},
-	optimization: {
-		splitChunks: {
-			chunks: "all"
-		}
-	},
-	stats: {
-		colors: true,
-		hash: false,
-		version: false,
-		timings: true,
-		assets: true,
-		chunks: false,
-		modules: false,
-		children: false
-	},
-	plugins: [
-		new HtmlWebpackPlugin({ template: "src/index.html" })
-	]
-}
+		},
+		mode: "development",
+		devServer: {
+			open: false,
+			port: 3000,
+			historyApiFallback: true
+		},
+		optimization: {
+			splitChunks: {
+				chunks: "all"
+			}
+		},
+		stats: {
+			colors: true,
+			hash: false,
+			version: false,
+			timings: true,
+			assets: true,
+			chunks: false,
+			modules: false,
+			children: false
+		},
+		plugins: [
+			new HtmlWebpackPlugin({ template: "src/index.html" })
+		]
+	}
+
+	return webpackConfig;
+};
