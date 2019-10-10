f7e4e63304c616de75b06d23a6ed3f722f974bb8
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-10 10:05:48 +0800

    Modified to functional components

--- src/home/index.js
+++ src/home/index.js
@@ -1,14 +1,23 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
+import { useDispatch } from 'react-redux'
 
 import style from "./index.scss";
+import { getGithubInfo } from "./actions";
 
-export default class Home extends Component {
-	render () {
+
+export default function Home() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getGithubInfo());
+	});
+
+	// render () {
 		return(
 			<div>
 				<p className={style.test}>Home Component loaded!</p>
 				<div className="spinner round"></div>
 			</div>
 		);
-	}
+	// }
 }
