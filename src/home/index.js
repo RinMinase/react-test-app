d2bea6e648f2d34b54995d905ed179a0f2c8e834
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-10 10:29:50 +0800

    Removed class declarations

--- src/home/index.js
+++ src/home/index.js
@@ -1,23 +1,20 @@
-import React, { Component, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from 'react-redux'
 
 import style from "./index.scss";
 import { getGithubInfo } from "./actions";
-
 
 export default function Home() {
 	const dispatch = useDispatch();
 
 	useEffect(() => {
 		dispatch(getGithubInfo());
-	});
+	}, []);
 
-	// render () {
-		return(
-			<div>
-				<p className={style.test}>Home Component loaded!</p>
-				<div className="spinner round"></div>
-			</div>
-		);
-	// }
+	return(
+		<div>
+			<p className={style.test}>Home Component loaded!</p>
+			<div className="spinner round"></div>
+		</div>
+	);
 }
