727455600e73e549dee83b7751ea38ad58c56fbe
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-10 11:42:30 +0800

    Removed script styling in navbar

--- src/core/navbar/index.js
+++ src/core/navbar/index.js
@@ -1,33 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { makeStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
 import MenuIcon from "@material-ui/icons/Menu"
 
-const useStyles = makeStyles(theme => ({
-	root: {
-		flexGrow: 1,
-	},
-	menuButton: {
-		marginRight: theme.spacing(2),
-	},
-	title: {
-		flexGrow: 1,
-	},
-}));
+import style from "./index.scss";
 
 export default function Navbar() {
-	const classes = useStyles();
-
 	return(
-		<div className={classes.root}>
+		<div>
 			<AppBar position="static">
 				<Toolbar>
-					<IconButton edge="start" color="inherit" className={classes.menuButton}>
+					<IconButton edge="start" color="inherit" className={style.menuButton}>
 						<MenuIcon />
 					</IconButton>
-					<Typography variant="h6" className={classes.title}>React Test App</Typography>
+					<Typography variant="h6" className={style.title}>React Test App</Typography>
 
 					<Link to="/home"><Button>Home</Button></Link>
 					<Link to="/login"><Button>Login</Button></Link>
